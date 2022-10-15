2b4f2896e10ea5f641d4f1b28c876469e59a9281
ParkHyunKyeong <kj3477417@gmail.com>
2022-10-15 21:25:18 +0900

    feature: 영화 목록 API 연결 및 Lint 규칙에서 @typescript-eslint/no-floating-promises 비활성화 처리 - axios 라이브러리 설치 - 영화 목록 조회 API 연결 - @typescript-eslint/no-floating-promises 규칙 비활성화: useEffect 내부에서 영화 목록 조회 API 호출 함수를 실행하기 위해 비활성화 - env 환경변수에 REACT_APP_API_KEY, REACT_APP_ACCESS_TOKEN 추가 - interfaces 폴더 내부에 Movie interface 생성

---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id: number;
+  backdrop_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+}
--- src/pages/movieList.tsx
+++ src/pages/movieList.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { Movie } from '../interfaces';
 import MovieTemplate from '../components/template/MovieTemplate';
 
+const API_KEY = process.env.REACT_APP_API_KEY as string;
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN as string;
+const API_URL = `https://api.themoviedb.org/4/list/1?page=1&api_key=${API_KEY}`;
+
 export default function MovieList(): React.ReactElement {
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+  const [description, setDescription] = useState('');
+  useEffect(() => {
+    const getMovieList = async (): Promise<void> => {
+      await axios
+        .get(API_URL, {
+          headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+        })
+        .then((response) => {
+          const {
+            data: { description: apiDescription, results },
+          } = response;
+
+          const responseMovieList: Movie[] = results.map((movieData: any) => ({
+            key: movieData.id,
+            id: movieData.id,
+            backdrop_path: movieData.backdrop_path,
+            release_date: movieData.release_date,
+            title: movieData.title,
+            vote_average: movieData.vote_average,
+            vote_count: movieData.vote_count,
+          }));
+          setMovieList(responseMovieList);
+          setDescription(apiDescription);
+        })
+        .catch((err: Error): void => console.error(err));
+    };
+    getMovieList();
+  }, []);
   return (
     <MovieTemplate title="영화 목록">
-      <div>영화 목록 페이지네이션 구현</div>
+      <div>
+        <div>Description</div>
+        <div>{description}</div>
+        <div>MovieList</div>
+        <div>
+          {movieList.map((movie: Movie) => (
+            <div key={movie.id}>
+              <div>Title: {movie.title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </MovieTemplate>
   );
 }
