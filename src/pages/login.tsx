68fb547290dc60a9cdb5ed3500a06f9ffa2c044f
ParkHyunKyeong <kj3477417@gmail.com>
2022-10-15 16:01:25 +0900

    feature: 이메일, 비밀번호 입력폼 구현 및 영화 목록 조회 페이지 이동 구현 - 이메일 유효성 검사 추가 - 비밀번호 유효성 검사는 미진행. 추후 구현 - 이메일 유효성 검사에서 통과하면, 영화 목록 조회 페이지로 이동

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(): React.ReactElement {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+
+  const validateEmail = (emailInput: string): boolean => {
+    const regExp = /^([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    return regExp.test(emailInput);
+  }
+
+  const onClickLogin = (): void => {
+    if (!validateEmail(email)) alert('이메일 형식이 올바르지 않습니다.');
+    else {
+      alert(`이메일: ${email} / 패스워드: ${password}`);
+      navigate('/movie/list');
+    }
+  }
   return (
     <div>
       <h1>로그인</h1>
+      <form>
+        <input name="email" placeholder="Email address" value={email} onChange={onChangeEmail} />
+        <input name="password" placeholder="Password" value={password} onChange={onChangePassword} />
+        <button type="button" onClick={onClickLogin}>
+          로그인
+        </button>
+      </form>
     </div>
   );
-  };
+};
